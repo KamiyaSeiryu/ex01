e7ab568b603be306dbd0f0b89796f0dcf9222764
KamiyaSeiryu <136040620+KamiyaSeiryu@users.noreply.github.com>
2023-06-14 13:51:54 +0900

    Greetings added

--- RandomDice.java
+++ RandomDice.java
@@ -1,15 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 class RandomDice{
-public static void main(String args[]){
-int x;
-int num = 0;
-Random Ran = new Random();
-System.out.println("Rolling dice...");
-for(int i=1; i<3; i++){
-x = Ran.nextInt(6) +1;
-System.out.println("Die "+i+": "+x);
-num += x;
-}
-System.out.println("Total value: "+num);
-}
+    public static void main(String args[]){
+	int x;
+	int num = 0;
+	Random Ran = new Random();
+
+	Scanner Scan = new Scanner(System.in);
+	System.out.println("What is your name?");
+	System.out.print(">");
+	String name = Scan.next();
+	System.out.println("Hello, "+name+"!");
+	
+	
+	System.out.println("Rolling dice...");
+	for(int i =1; i<3;i++){
+	    x = Ran.nextInt(6) + 1;
+	    System.out.println("Die "+i+": "+x);
+	    num += x;
+	}
+	System.out.println("Total value: "+num);
+    }
 }
